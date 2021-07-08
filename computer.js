e0ac6ff2be143b4243b2304ffc06bd44ecdae942
Rhia Singh <rhia.singh@macaulay.cuny.edu>
2021-07-08 12:55:50 -0400

    files for website

---
+++ computer.js
@@ -0,0 +1,20 @@
+function draw_computer() {
+
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    var context = canvas.getContext('2d');
+
+    context.fillRect(0,30,500,300);
+    context.clearRect(10,37,480,280);
+
+	context.font = "60px Comic Sans MS";
+	context.fillStyle = "red";
+	context.textAlign = "center";
+	context.fillText("Welcome", 250, 150);
+
+	context.fillStyle = "black";
+	context.fillRect(250,330,20,40);
+	context.fillRect(100,370,320,20);
+  }
+
+}
