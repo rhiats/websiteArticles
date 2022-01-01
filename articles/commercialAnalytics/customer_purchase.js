34656b6530d2c91cf49ff70f694bf8e98c318bee
Rhia Singh <rhia.singh@macaulay.cuny.edu>
2022-01-01 13:41:30 -0500

    add bars to show the data

--- articles/commercialAnalytics/customer_purchase.js
+++ articles/commercialAnalytics/customer_purchase.js
@@ -11,7 +11,12 @@ function customer_purchase_bar_chart() {
 
     const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
 
-    
+    svg.selectAll("rect").data(dataset).enter().append("rect").attr("x", (d, i) => i * 30)
+       .attr("y", (d, i) => h - 3 * d)
+       .attr("width", 25)
+       .attr("height", (d, i) => d * 3)
+       .attr("fill", "navy")
+       .attr("class", "bar")
   }
 
 }
