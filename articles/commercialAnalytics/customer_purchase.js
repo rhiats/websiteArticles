1d260000dabbd3ebf858064985ab2b40f3b8d716
Rhia Singh <rhia.singh@macaulay.cuny.edu>
2022-01-01 14:07:37 -0500

    move bar chart to the center of the webpage

--- articles/commercialAnalytics/customer_purchase.js
+++ articles/commercialAnalytics/customer_purchase.js
@@ -1,22 +1,19 @@
 function customer_purchase_bar_chart() {
 
-  var canvas = document.getElementById('canvas');
+  var canvas = document.getElementById('svg');
 
-  if (canvas.getContext) {
-    var context=canvas.getContext('2d');
-    const dataset = [99, 68, 2];
+  const dataset = [99, 68, 2];
 
-    const w = 500;
-    const h = 500;
+  const w = 500;
+  const h = 500;
 
-    const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+  const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
 
-    svg.selectAll("rect").data(dataset).enter().append("rect").attr("x", (d, i) => i * 30)
-       .attr("y", (d, i) => h - 3 * d)
-       .attr("width", 25)
-       .attr("height", (d, i) => d * 3)
-       .attr("fill", "navy")
-       .attr("class", "bar")
-  }
+  svg.selectAll("rect").data(dataset).enter().append("rect").attr("x", (d, i) => i * 30)
+     .attr("y", (d, i) => h - 3 * d)
+     .attr("width", 25)
+     .attr("height", (d, i) => d * 3)
+     .attr("fill", "navy")
+     .attr("class", "bar")
 
 }
