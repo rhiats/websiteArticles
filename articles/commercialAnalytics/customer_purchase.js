875de71b57ae8c7bdb104d70b32444091b2b9081
Rhia Singh <rhia.singh@macaulay.cuny.edu>
2022-01-15 15:36:06 -0500

    add bar graph

--- articles/commercialAnalytics/customer_purchase.js
+++ articles/commercialAnalytics/customer_purchase.js
@@ -1,38 +1,59 @@
 function customer_purchase_bar_chart() {
 
-  const dataset = [
-    { name: "Sign-In", value: 99}, 
-    { name: "Revisit", value: 68},
-    { name: "Log-In", value: 2}
+  //reference: https://bl.ocks.org/d3noob/08cc6159b9315e20e74a72e85a50dd3e
+
+  // set the dimensions and margins of the graph
+  var margin = {top: 20, right: 20, bottom: 30, left: 40},
+  width = 960 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
+
+  // set the ranges
+  var x = d3.scaleBand()
+        .range([0, width])
+        .padding(0.1);
+  var y = d3.scaleLinear()
+        .range([height, 0]);
+        
+  // append the svg object to the body of the page
+  // append a 'group' element to 'svg'
+  // moves the 'group' element to the top left margin
+  var svg = d3.select("body").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", 
+        "translate(" + margin.left + "," + margin.top + ")");
+
+  const data = [
+    { name: 'Sign-In', value: 99 },
+    { name: 'Revisit', value: 68 },
+    { name: 'Log-In', value: 2 }
   ];
 
-  const w = 500;
-  const h = 400;
+  // Scale the range of the data in the domains
+  x.domain(data.map(function(d) { return d.name; }));
+  y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
-  const svg = d3.select("body").append("svg").attr("width", w).attr("height", h).attr("class","graph");
-
-  const x = d3.scaleBand().domain(d3.range(dataset.length)).range([0,w])
-
-  const y = d3.scaleLinear().domain([0,100]).range([0,400])
-
-  svg.selectAll("rect").data(dataset).enter()
+  // append the rectangles for the bar chart
+  svg.selectAll(".bar")
+    .data(data).enter()
     .append("rect")
-    .attr("x", (d, i) => x(i))
-    .attr("y", (d) => y(d.value))
-    .attr("width", 50)
-    .attr("height", (d, i) => d * 3)
-    .attr("fill", "navy")
     .attr("class", "bar")
+    .attr("x", function(d) { return x(d.name); })
+    .attr("width", x.bandwidth())
+    .attr("y", function(d) { return y(d.value); })
+    .attr("height", function(d) { return height - y(d.value); })
+    .attr("fill","navy")
     .append("title")
-    .text((d)=>(d)+" %")
-    .attr("class","bar")
+    .text((d)=>(d.value)+" %")
 
+  // add the x Axis
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
 
-  function xAxis(g){
-    g.call(d3.axisBottom(x).tickFormat(i => dataset[i].name)
-  }
-
-  svg.append('g').call(xAxis);
-  svg.node();
+  // add the y Axis
+  svg.append("g")
+    .call(d3.axisLeft(y));
 
 }
