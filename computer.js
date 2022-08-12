0527c616aadf3ef405566f049a85e85916004dc1
Rhia Singh <rhia.singh@macaulay.cuny.edu>
2022-08-12 15:11:57 -0400

    Style updates to the site

--- computer.js
+++ computer.js
@@ -6,15 +6,15 @@ function draw_computer() {
     var context=canvas.getContext('2d');
     var i=0;
     var speed=50;
-    var txt="Welcome"
+    var txt="Tech Thoughts"
 
     context.fillRect(0,30,500,300);
     context.clearRect(10,37,480,280);
 
-  	context.font = "60px Comic Sans MS";
-  	context.fillStyle = "red";
+  	context.font = "60px Sans-serif";
+  	context.fillStyle = "navy";
   	context.textAlign = "center";
-  	context.fillText("Welcome", 250, 150);
+  	context.fillText("Tech Thoughts", 250, 150);
 
   	context.fillStyle = "black";
   	context.fillRect(250,330,20,40);
