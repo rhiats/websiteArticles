96d0fc94391251b7d9c0b4da022ced920c26e5b4
Rhia Singh <rhia.singh22@gmail.com>
2025-06-14 15:02:40 -0400

    Add chat box code

--- websiteChat.js
+++ websiteChat.js
@@ -1,4 +1,4 @@
-// firebase-chat.js
+// websiteChat.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getDatabase, ref, push, onChildAdded } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
