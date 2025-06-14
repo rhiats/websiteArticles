a7f6666f28687a720316e465faa227420be68533
Rhia Singh <rhiasingh@Rhias-MacBook-Pro.local>
2025-06-14 11:58:44 -0400

    Add likes to recomendation article page

--- likes.js
+++ likes.js
@@ -18,7 +18,7 @@ const db = getDatabase(app);
 // ✅ List of like IDs (add more if needed)
 const likeIDs = ["airbnb_nyc", "lifeExpectancy", "breastCancerClassification",
   "CADClassification", "CommercialAnalytics", "privacy", "segmentationDiabetes",
-  "DiabeticNB","PopGrowth","LRSupermarketSales"
+  "DiabeticNB","PopGrowth","LRSupermarketSales","MovieRec"
 ];
 
 // Load and update counts on page load
